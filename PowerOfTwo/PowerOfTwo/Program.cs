bb718cb2706e216b33ebf2dba13dd92a6da8e2c4
akustanovich <akustanovich@users.noreply.github.com>
2019-10-17 15:56:12 -0400

    Add files via upload

---
+++ PowerOfTwo/PowerOfTwo/Program.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace PowerOfTwo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length > 0)
+      {
+        var result = Power(10);
+        Console.WriteLine($" Power of 2 for {args[0]} is {result}");
+      }
+      else
+      {
+        Console.WriteLine("Missing arguments");
+      }
+
+      double Power(int powerRaised)
+      {
+
+        if (powerRaised != 0)
+        {
+          return (2 * Power(powerRaised - 1));
+        }
+        else
+        {
+          return 1;
+        }
+      }
+
+    }
+
+  }
+}
